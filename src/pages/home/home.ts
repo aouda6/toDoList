b103828fe24f59cf4fe8b5ea594f21a016ff0d86
Abdulla Ouda <aouda6@uwo.ca>
2017-11-14 21:13:35 -0500

    Added + Improved Functionality
    Crossing out completed tasks works. "Delete All" button added. "Select All" button is in progress.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,18 +5,17 @@ import {AngularFireDatabase} from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
+
 export class HomePage {
 
   arrData = [];
   myInput;
-  style = 'color=primary'
 
   constructor(public navCtrl: NavController, private fdb: AngularFireDatabase) {
     this.fdb.list("/myItems").subscribe(_data => {
       this.arrData = _data;
-      console.log(this.arrData );
     });
   }
 
@@ -29,19 +28,21 @@ export class HomePage {
     this.myInput = '';
   }
 
-  delete(i) {
+  deleteItem(i) {
     this.fdb.list("/myItems").remove(this.arrData[i].$key);
   }
 
   changeCrossOut(i) {
-    if (this.arrData[i].done)
-      this.style = 'color=danger';
-    else
-      this.style = 'color=primary';
-
     this.fdb.object("/myItems/"+this.arrData[i].$key).update({done: this.arrData[i].done});
-
   }
 
+  deleteAll() {
+    this.fdb.list("/myItems").remove();
+  }
+
+  SelectAll() {
+    this.fdb.object("/myItems").update();
+
+  }
 
 }
