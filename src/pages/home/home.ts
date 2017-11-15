dbfa1cb1818435e764fbaa6b3aa7b0595aac8c8e
Abdulla Ouda <aouda6@uwo.ca>
2017-11-14 23:22:35 -0500

    Select All + Comments
    Added functionality to De/Select All button and added comments to the code.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
 import {AngularFireDatabase} from 'angularfire2/database';
+import * as firebase from "firebase";
 
 @Component({
   selector: 'page-home',
@@ -12,13 +13,16 @@ export class HomePage {
 
   arrData = [];
   myInput;
+  selected = true;
 
   constructor(public navCtrl: NavController, private fdb: AngularFireDatabase) {
-    this.fdb.list("/myItems").subscribe(_data => {
+    // This monitors and retrieves any changes in the database.
+    this.fdb.list("/myItems").subscribe((_data) => {
       this.arrData = _data;
     });
   }
 
+  // A method to add an item to the database.
   btnAddClicked() {
     this.fdb.list("/myItems").push({
         name: this.myInput,
@@ -32,6 +36,7 @@ export class HomePage {
     this.fdb.list("/myItems").remove(this.arrData[i].$key);
   }
 
+  // This method updates the 'done' property of a particular item.
   changeCrossOut(i) {
     this.fdb.object("/myItems/"+this.arrData[i].$key).update({done: this.arrData[i].done});
   }
@@ -40,9 +45,14 @@ export class HomePage {
     this.fdb.list("/myItems").remove();
   }
 
-  SelectAll() {
-    this.fdb.object("/myItems").update();
-
+  // This methods updates all 'done' properties of the items in order to select or deselect all items.
+  selection(flag) {
+    this.selected = !flag;
+    firebase.database().ref('/myItems').once('value', (data)=>{
+      data.forEach((rec) => {
+        this.fdb.object("/myItems/"+rec.key).update({done: flag});
+      })
+    });
   }
 
 }
